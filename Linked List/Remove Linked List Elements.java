e3c0924def769d1ea68c2b92126b49dcabcfb332
AmanSirohi31 <amansirohi3105@gmail.com>
2024-07-23 00:08:54 +0530

    Update Remove Linked List Elements.java

--- Linked List/Remove Linked List Elements.java
+++ Linked List/Remove Linked List Elements.java
@@ -3,30 +3,11 @@ class Solution {
         if(head==null){
             return null;
         }
-        ListNode prev = null;
-        ListNode cur = head;
-        while(cur!=null){
-            if(cur.val==val){
-                if(cur==head){
-                    while(cur.val==val){
-                        cur=cur.next;
-                        if(cur==null){
-                            return null;
-                        }
-                        head=cur;
-                    }
-                }else{
-                    cur=cur.next;
-                    if(cur==null){
-                        prev.next=cur;
-                    }
-                }
-            }else{
-                if(prev!=null)prev.next=cur;
-                prev=cur;
-                cur=cur.next;
-            }
+        head.next = removeElements(head.next, val);
+        if(head.val==val){
+            return head.next;
+        }else{
+            return head;
         }
-        return head;
     }
 }
